533bf1f24cda750622678f74c4174516ee4f8e56
FabioMota97 <fabiomota1997@gmail.com>
2020-04-13 11:44:39 +0100

    terceiro commit

--- MC1.02 apagar/mc.py
+++
@@ -1,6 +0,0 @@
-from app import app, db
-from app.models import Jogador, Equipa, Jogo, Competicao
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'Jogador': Jogador, 'Equipa': Equipa, 'Competicao': Competicao, 'Jogo': Jogo}
